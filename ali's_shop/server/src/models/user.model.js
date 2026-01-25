a86a424596ba30929d982921cc36a4507133a421
kheirkheberli <ali.xov08@gmail.com>
2026-01-25 11:49:39 +0400

    user modelSchema created

---
+++ ali's_shop/server/src/models/user.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: [true, 'Ad və Soyadınızı daxil edin'],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, 'E-mail ünvan daxil edin'],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Zəhmət olmasa düzgün email daxil edin']
+    },
+    phone: {
+        type: String,
+        required: [true, 'Mobil nömrənizi daxil edin'],
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Şifrənizi daxil edin'],
+        minlength: [8, 'Şifrə 8 simvoldan az olmamalıdır'],
+        select: false
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
