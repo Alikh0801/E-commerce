5a97663508e7d9c7a24c68b8e89f926904ebaf8c
kheirkheberli <ali.xov08@gmail.com>
2026-02-25 12:53:33 +0400

    vercel deployment settings

--- az_perfumery_v2/server/app.js
+++ az_perfumery_v2/server/app.js
@@ -8,16 +8,21 @@ const app = express();
 dbConnect();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.NODE_ENV === 'production'
+        ? ['https://az-perfumery-v2-uvpk.vercel.app']
+        : 'http://localhost:5173',
     credentials: true
 }));
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api', router)
+app.use('/api', router);
 
-app.listen(config.port, () => {
-    console.log('Server is running');
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(process.env.PORT || 3500, () => {
+        console.log('Server is running');
+    });
+};
 
-})
+module.exports = app;
